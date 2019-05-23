6146a0ca788b331b7d68c7659ed706e7eaa38aad
Ian Murphy <ian@rockfortengineering.com>
2019-05-23 11:55:48 +0100

    Library update.

--- setup.sh
+++ setup.sh
@@ -6,12 +6,14 @@
 # sudo modprobe vcan
 # sudo apt-get install can-utils
 
+# NOTE: the above have been tested with PEAK-CAN hardware only
 
-# vcan0 for testing without hardware
+
+# can0 hardware at 500kbps
 sudo ip link set can0 type can bitrate 500000
 sudo ip link set can0 up
 
-# can0 hardware at 500kbps
+# vcan0 for testing without hardware
 sudo ip link add dev vcan0 type vcan
 sudo ip link set vcan0 up
 
